23bc3f11f98b038522823181b25b9bb28b0f6e1a
Atieh <atieh.rahbari@gmail.com>
2021-10-05 15:24:23 +0330

    modify StorageDao.java DataBaseAccess.java StorageDaoImp.java

--- src/Storage/DataBaseAccess.java
+++ src/Storage/DataBaseAccess.java
@@ -5,6 +5,17 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DataBaseAccess {
+    private Connection connection = null;
 
+    public DataBaseAccess() throws SQLException , ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/hw6_cab_req"
+                , "root", "root");
+
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
 
 }
--- src/Storage/StorageDao.java
+++ src/Storage/StorageDao.java
@@ -7,5 +7,11 @@ import Driver.models.Driver;
 import java.sql.SQLException;
 
 public interface StorageDao {
+    int savePassenger(Passenger passenger) throws SQLException, ClassNotFoundException;
+
+    int saveDriver(Driver driver) throws SQLException, ClassNotFoundException;
+
+    int saveVehicle(Vehicle vehicle) throws SQLException, ClassNotFoundException;
+
 
 }
